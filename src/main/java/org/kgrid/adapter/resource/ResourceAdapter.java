25da091ed6333675de5ad284d03aafa778c11602
ngittlen <nggittle@med.umich.edu>
2020-12-01 16:33:01 -0500

    Refactored to move get methods to the activator and have this adapter create executors instead of doing everything itself. Also added circleci build config.

--- src/main/java/org/kgrid/adapter/resource/ResourceAdapter.java
+++ src/main/java/org/kgrid/adapter/resource/ResourceAdapter.java
@@ -1,26 +1,18 @@
 package org.kgrid.adapter.resource;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import org.apache.commons.lang3.StringUtils;
 import org.kgrid.adapter.api.ActivationContext;
 import org.kgrid.adapter.api.Adapter;
 import org.kgrid.adapter.api.AdapterException;
 import org.kgrid.adapter.api.Executor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-@Controller
 public class ResourceAdapter implements Adapter {
 
-    public static final Map<URI, ArtifactEntry> activatedObjectArtifacts = new HashMap<>();
     private static ActivationContext context;
 
     @Override
@@ -35,52 +27,34 @@ public class ResourceAdapter implements Adapter {
 
     @Override
     public Executor activate(URI absoluteLocation, URI endpointUri, JsonNode deploymentSpec) {
+        final List<String> artifacts = new ArrayList<>();
         JsonNode artifactNode = deploymentSpec.at("/artifact");
         ArrayList<String> artifactLocations = new ArrayList<>();
         if (artifactNode.isArray()) {
             artifactNode.forEach(file -> {
-                artifactLocations.add(file.asText());
+                artifacts.add(file.asText());
             });
         } else {
-            artifactLocations.add(artifactNode.asText());
+            artifacts.add(artifactNode.asText());
         }
-        activatedObjectArtifacts.put(endpointUri, new ArtifactEntry(artifactLocations, absoluteLocation));
-        return null;
+        return new Executor() {
+            @Override
+            public Object execute(Object inputs, String inputMimetype) {
+                if(inputs == null){
+                    return artifacts;
+                }
+                if(artifacts.contains(inputs)) {
+                    return context.getBinary(
+                            URI.create(absoluteLocation + "/" + inputs));
+                } else {
+                    throw new AdapterException("Requested resource " + inputs + " is not available.");
+                }
+            }
+        };
     }
 
     @Override
     public String status() {
         return "UP";
-    }
-
-    @GetMapping(value = "{naan}/{name}/{apiVersion}/{endpoint}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<String>> getResourceList(HttpServletRequest request) {
-        String requestUri = request.getRequestURI();
-        String trimmedUri = requestUri.substring(1, requestUri.length() - 1);
-        ArtifactEntry artifactEntry = activatedObjectArtifacts.get(URI.create(trimmedUri));
-        if (artifactEntry == null || artifactEntry.getArtifacts().size() == 1 && artifactEntry.getArtifacts().get(0).equals("")) {
-            throw new AdapterException("No artifacts specified for endpoint: " + trimmedUri);
-        } else {
-            return new ResponseEntity<>(artifactEntry.getArtifacts(), HttpStatus.OK);
-        }
-    }
-
-    @GetMapping(value = "{naan}/{name}/{apiVersion}/{endpoint}/**", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public ResponseEntity<byte[]> getResource(HttpServletRequest request,
-                                              @PathVariable String naan,
-                                              @PathVariable String name,
-                                              @PathVariable String apiVersion,
-                                              @PathVariable String endpoint) {
-        URI endpointUri = URI.create(naan + "/" + name + "/" + apiVersion + "/" + endpoint);
-        ArtifactEntry artifactEntry = activatedObjectArtifacts.get(
-                endpointUri);
-        String artifactName = StringUtils.substringAfterLast(request.getRequestURI().substring(1), endpoint + "/");
-        if (artifactEntry != null && artifactEntry.getArtifacts().size() > 0
-                && artifactEntry.getArtifacts().contains(artifactName)) {
-            byte[] bytes = context.getBinary(
-                    URI.create(artifactEntry.getBaseLocation() + "/" + artifactName));
-            return new ResponseEntity<>(bytes, HttpStatus.OK);
-        }
-        throw new AdapterException("Object at " + endpointUri + "/" + artifactName + " is not available");
     }
 }
--- src/test/java/org/kgrid/adapter/resource/ResourceAdapterTest.java
+++ src/test/java/org/kgrid/adapter/resource/ResourceAdapterTest.java
@@ -8,142 +8,105 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kgrid.adapter.api.ActivationContext;
 import org.kgrid.adapter.api.AdapterException;
+import org.kgrid.adapter.api.Executor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletRequest;
 
-import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ResourceAdapterTest {
-    public static final String ENGINE_NAME = "resource";
-    public static final String NAAN = "naan";
-    public static final String NAME = "name";
-    public static final String API_VERSION = "apiVersion";
-    public static final String VERSION = "version";
-    public static final String ENDPOINT = "endpoint";
-    public static final String ARTIFACT_NAME1 = "hello.txt";
-    public static final String ARTIFACT_NAME2 = "goodbye.txt";
-    public static final URI ENDPOINT_URI = URI.create("/" + NAAN + "/" + NAME + "/" + API_VERSION + "/" + ENDPOINT + "/");
-    public static final String ENDPOINT_MAP_KEY = ENDPOINT_URI.getPath().substring(1, ENDPOINT_URI.getPath().length() - 1);
-    public static final String REQUEST_URI = ENDPOINT_URI + ARTIFACT_NAME1;
-    public static final String ARTIFACT_CONTENTS = "hello world!";
+  public static final String ENGINE_NAME = "resource";
+  public static final String NAAN = "naan";
+  public static final String NAME = "name";
+  public static final String API_VERSION = "apiVersion";
+  public static final String VERSION = "version";
+  public static final String ENDPOINT = "endpoint";
+  public static final String ARTIFACT_NAME1 = "hello.txt";
+  public static final String ARTIFACT_NAME2 = "goodbye.txt";
+  public static final URI ENDPOINT_URI =
+      URI.create("/" + NAAN + "/" + NAME + "/" + API_VERSION + "/" + ENDPOINT + "/");
+  public static final String ENDPOINT_MAP_KEY =
+      ENDPOINT_URI.getPath().substring(1, ENDPOINT_URI.getPath().length() - 1);
+  public static final String REQUEST_URI = ENDPOINT_URI + ARTIFACT_NAME1;
+  public static final String ARTIFACT_CONTENTS = "hello world!";
 
-    @Mock
-    ActivationContext context;
-    @InjectMocks
-    ResourceAdapter adapter;
-    URI locationUri = URI.create(NAAN + "/" + NAME + "/" + VERSION);
-    private final String singleArtifactDeploymentSpec =
-            "{\"artifact\":\"" + ARTIFACT_NAME1 + "\",\"engine\":\"" + ENGINE_NAME + "\"}";
-    private final String twoArtifactDeploymentSpec = "{\"artifact\":[\"" + ARTIFACT_NAME1 + "\", \"" + ARTIFACT_NAME2
-            + "\"],\"engine\":\"" + ENGINE_NAME + "\"}";
+  @Mock ActivationContext context;
+  @InjectMocks ResourceAdapter adapter;
+  URI locationUri = URI.create(NAAN + "/" + NAME + "/" + VERSION);
+  private final String singleArtifactDeploymentSpec =
+      "{\"artifact\":\"" + ARTIFACT_NAME1 + "\",\"engine\":\"" + ENGINE_NAME + "\"}";
+  private final String twoArtifactDeploymentSpec =
+      "{\"artifact\":[\""
+          + ARTIFACT_NAME1
+          + "\", \""
+          + ARTIFACT_NAME2
+          + "\"],\"engine\":\""
+          + ENGINE_NAME
+          + "\"}";
 
+  @After
+  public void tearDown() {}
 
-    @After
-    public void tearDown() {
-        ResourceAdapter.activatedObjectArtifacts.clear();
-    }
+  @Test
+  public void reportsResourceEngineType() {
+    assertEquals(ENGINE_NAME, adapter.getEngines().get(0));
+  }
 
-    @Test
-    public void reportsResourceEngineType() {
-        assertEquals(ENGINE_NAME, adapter.getEngines().get(0));
-    }
+  @Test
+  public void reportsStatusUp() {
+    assertEquals("UP", adapter.status());
+  }
 
-    @Test
-    public void reportsStatusUp() {
-        assertEquals("UP", adapter.status());
-    }
+  @Test
+  public void getResource_CanActivateObjects() throws JsonProcessingException {
+    Executor ex = activateDeploymentSpec(singleArtifactDeploymentSpec);
+    List<String> artifacts = new ArrayList<>();
+    artifacts.add("hello.txt");
+    assertEquals(artifacts, ex.execute(null, null));
+  }
 
-    @Test
-    public void getResource_CanActivateObjects() throws JsonProcessingException {
-        activateDeploymentSpec(singleArtifactDeploymentSpec);
+  @Test
+  public void getResource_objectArrayActivate() throws JsonProcessingException {
+    Executor ex = activateDeploymentSpec(twoArtifactDeploymentSpec);
+    List<String> artifacts = new ArrayList<>();
+    artifacts.add("hello.txt");
+    artifacts.add("goodbye.txt");
+    assertEquals(artifacts, ex.execute(null, null));
+  }
 
-        ArtifactEntry artifactEntry = ResourceAdapter.activatedObjectArtifacts.get(
-                URI.create(ENDPOINT_MAP_KEY));
-        assertEquals(1, artifactEntry.getArtifacts().size());
-        assertTrue(artifactEntry.getArtifacts().contains(ARTIFACT_NAME1));
-    }
+  @Test
+  public void getResource_returnActivatedArtifact() throws JsonProcessingException {
+    adapter.initialize(context);
+    when(context.getBinary(URI.create(locationUri + "/" + ARTIFACT_NAME1)))
+        .thenReturn(ARTIFACT_CONTENTS.getBytes());
+    Executor ex = activateDeploymentSpec(singleArtifactDeploymentSpec);
 
-    @Test
-    public void getResource_objectArrayActivate() throws JsonProcessingException {
-        activateDeploymentSpec(twoArtifactDeploymentSpec);
+    assertEquals(
+        ARTIFACT_CONTENTS,
+        new String((byte[]) ex.execute("hello.txt", null), StandardCharsets.UTF_8));
+  }
 
-        ArtifactEntry artifactEntry = ResourceAdapter.activatedObjectArtifacts.get(URI.create(ENDPOINT_MAP_KEY));
-        assertEquals(2, artifactEntry.getArtifacts().size());
-        assertTrue(artifactEntry.getArtifacts().contains(ARTIFACT_NAME1));
-        assertTrue(artifactEntry.getArtifacts().contains(ARTIFACT_NAME2));
-    }
+  @Test
+  public void getResource_unactivatedObjectThrowsException() throws JsonProcessingException {
+    adapter.initialize(context);
+    Executor ex = activateDeploymentSpec(singleArtifactDeploymentSpec);
+    AdapterException adapterException =
+        assertThrows(AdapterException.class, () -> ex.execute("notHere.txt", null));
+    assertEquals("Requested resource notHere.txt is not available.", adapterException.getMessage());
+  }
 
-    @Test
-    public void getResource_returnActivatedArtifact() throws JsonProcessingException {
-        adapter.initialize(context);
-        when(context.getBinary(URI.create(locationUri + "/" + ARTIFACT_NAME1))).thenReturn(ARTIFACT_CONTENTS.getBytes());
-        activateDeploymentSpec(singleArtifactDeploymentSpec);
-
-        HttpServletRequest request = new MockHttpServletRequest("GET", REQUEST_URI);
-        ResponseEntity<byte[]> response = adapter.getResource(request, NAAN, NAME, API_VERSION, ENDPOINT);
-
-        assertEquals(ARTIFACT_CONTENTS,
-                new String(Objects.requireNonNull(response.getBody()), StandardCharsets.UTF_8));
-    }
-
-    @Test
-    public void getResource_unactivatedObjectThrowsException() throws JsonProcessingException {
-        adapter.initialize(context);
-        activateDeploymentSpec(singleArtifactDeploymentSpec);
-        HttpServletRequest request =
-                new MockHttpServletRequest("GET", ENDPOINT_URI + "missingFile.txt");
-        AdapterException adapterException = assertThrows(AdapterException.class, () ->
-                adapter.getResource(request, NAAN, NAME, API_VERSION, ENDPOINT));
-        assertEquals("Object at " + ENDPOINT_MAP_KEY + "/missingFile.txt is not available",
-                adapterException.getMessage());
-    }
-
-    @Test
-    public void getResourceList_returnsListOfArtifacts() throws JsonProcessingException {
-        activateDeploymentSpec(twoArtifactDeploymentSpec);
-        HttpServletRequest request = new MockHttpServletRequest("GET", ENDPOINT_URI.getPath());
-
-        ResponseEntity<List<String>> response = adapter.getResourceList(request);
-        assertEquals(2, Objects.requireNonNull(response.getBody()).size());
-        assertTrue(response.getBody().contains(ARTIFACT_NAME1));
-        assertTrue(response.getBody().contains(ARTIFACT_NAME2));
-    }
-
-    @Test
-    public void getResourceList_throwsAdapterExceptionIfListIsEmpty() throws JsonProcessingException {
-        String noArtifactDeploymentSpec = "{\"engine\":\"" + ENGINE_NAME + "\"}";
-        activateDeploymentSpec(noArtifactDeploymentSpec);
-        HttpServletRequest request = new MockHttpServletRequest("GET", ENDPOINT_URI.getPath());
-
-        AdapterException adapterException = assertThrows(AdapterException.class, () ->
-                adapter.getResourceList(request));
-        assertEquals("No artifacts specified for endpoint: " + ENDPOINT_MAP_KEY, adapterException.getMessage());
-    }
-
-    @Test
-    public void getResourceList_throwsAdapterExceptionIfListIsNull() {
-        HttpServletRequest request = new MockHttpServletRequest("GET", ENDPOINT_URI.getPath());
-
-        AdapterException adapterException = assertThrows(AdapterException.class, () ->
-                adapter.getResourceList(request));
-        assertEquals("No artifacts specified for endpoint: " + ENDPOINT_MAP_KEY, adapterException.getMessage());
-    }
-
-    private void activateDeploymentSpec(String deploymentSpecJson) throws JsonProcessingException {
-        JsonNode deploymentSpec = new ObjectMapper().readTree(
-                deploymentSpecJson);
-        adapter.activate(locationUri, URI.create(ENDPOINT_MAP_KEY), deploymentSpec);
-    }
+  private Executor activateDeploymentSpec(String deploymentSpecJson)
+      throws JsonProcessingException {
+    JsonNode deploymentSpec = new ObjectMapper().readTree(deploymentSpecJson);
+    return adapter.activate(locationUri, URI.create(ENDPOINT_MAP_KEY), deploymentSpec);
+  }
 }
